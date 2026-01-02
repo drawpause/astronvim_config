dd41086b92e9a43c0d8603260d939c1a5b907a0e
Henri Kuittinen <henri.kuittinen_ext@veikkaus.fi>
2026-01-02 22:16:07 +0200

    Colortheme Kanagawa

--- lua/community.lua
+++ lua/community.lua
@@ -8,6 +8,6 @@
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
-  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.colorscheme.kanagawa-nvim" },
   -- import/override with your plugins folder
 }
