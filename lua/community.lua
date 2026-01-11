7083482619e5db9f83ca5aad471aad3f7dd593ee
Henri Kuittinen <henri.kuittinen_ext@veikkaus.fi>
2026-01-11 18:06:52 +0200

    Add Go debugger

--- lua/community.lua
+++ lua/community.lua
@@ -9,5 +9,6 @@ return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.colorscheme.kanagawa-nvim" },
+  { import = "astrocommunity.pack.go" },
   -- import/override with your plugins folder
 }
